0fa59b5315c06509e174eb8567c080f124f8acdc
wangxuwei <wangxuwei84@gmail.com>
2012-02-13 21:29:51 +0800

    + add image support

--- assets/www/js/DialogTodo.js
+++ assets/www/js/DialogTodo.js
@@ -3,18 +3,35 @@
 	function DialogTodo(){};
   
 	// --------- Component Interface Implementation ---------- //
-	DialogTodo.prototype.build = function(data,config){
+	DialogTodo.prototype.create = function(data,config){
+		var c = this;
 		var id = null;
 		if(data && data.id){
 			id = data.id;
 		}
 		var dfd = $.Deferred();
 		brite.dm.get("todo",id).done(function(todo){
+			c.todo = todo;
 			var $e = $("#tmpl-DialogTodo").render(todo);
 			$("body").append("<div id='notTransparentScreen' class='dialogTodoScreen'></div>");
 			dfd.resolve($e);
 		});
+		
 		return dfd.promise();
+	}
+	
+	DialogTodo.prototype.init = function(data,config){
+		var c = this;
+		var $e = this.$element;
+		
+		var imagePickerInitDfd = brite.display("ImagePicker", {
+			image : c.todo?localStorage.getItem("image_"+c.todo.imageId):null
+		}, {
+			parent : $e.find(".imageContainer")
+		}).whenInit.done(function(imagePicker) {
+			c.imagePicker = imagePicker;
+		});
+		
 	}
 		
 	DialogTodo.prototype.postDisplay = function(data,config){
@@ -32,18 +49,59 @@
 			todo.startDate = $("#updateTodo").find("input[name='startDate']").val();
 			todo.endDate = $("#updateTodo").find("input[name='endDate']").val();
 			todo.status = $("#updateTodo").find("input[name='status']").val();
+			todo.imageId = $("#updateTodo").find("input[name='imageId']").val();
 			var id = $("#updateTodo").find("input[name='id']").val();
+			var image = c.imagePicker.getImage();
+			var imageUrl = c.imagePicker.getCurrentImageSrc();
+			var imageObj = null;
+			if(image.file){
+				imageObj = {};
+				imageObj.type = "local";
+				imageObj.ext = image.file.fileName.substring(image.file.fileName.lastIndexOf("."),image.file.fileName.length);
+			}
+			var imageDfd = $.Deferred();
 			if(id && id!=""){
-				brite.dm.update("todo", id, todo).done(function(){
-					c.close();
-					brite.display("Todo");
+				brite.dm.remove("image",todo.imageId).done(function(){
+					localStorage.removeItem("image_"+todo.imageId);
+					if(imageObj != null){
+						brite.dm.create("image",imageObj).done(function(imageId){
+							localStorage.setItem("image_"+imageId,c.imagePicker.getCurrentImageSrc());
+							imageDfd.resolve(imageId);
+						});
+					}else{
+						imageDfd.resolve(imageId);
+					}
+				});
+				imageDfd.done(function(imageId){
+					if(imageId != null){
+						todo.imageId = imageId;
+					}
+					brite.dm.update("todo", id, todo).done(function(){
+						c.close();
+						brite.display("Todo");
+					});
 				});
 			}else{
-				brite.dm.create("todo",todo).done(function(){
-					c.close();
-					brite.display("Todo");
+				if(imageObj != null){
+					brite.dm.create("image",imageObj).done(function(imageId){
+						localStorage.setItem("image_"+imageId,c.imagePicker.getCurrentImageSrc());
+						imageDfd.resolve(imageId);
+					});
+				}else{
+					imageDfd.resolve(imageId);
+				}
+				imageDfd.done(function(imageId){
+					if(imageId != null){
+						todo.imageId = imageId;
+					}
+					brite.dm.create("todo",todo).done(function(){
+						c.close();
+						brite.display("Todo");
+					});
 				});
+				
 			}
+			
 		});
 		
 		
--- assets/www/js/todoApp-daos.js
+++ assets/www/js/todoApp-daos.js
@@ -15,7 +15,8 @@ var _SQLiteDb;
 	);		
 	brite.registerDao("tag",new brite.dao.SQLiteDao("Tags","id",[{column:'name',dtype:'TEXT'}]));
 	brite.registerDao("tagtodo",new brite.dao.SQLiteDao("TagTodos","id",[{column:'tagId',dtype:'INTEGER'},{column:'todoId',dtype:'INTEGER'}]));	
-	brite.registerDao("todo",new brite.dao.SQLiteDao("Todos","id",[{column:'taskName',dtype:'TEXT'},{column:'startDate',dtype:'TEXT'},{column:'endDate',dtype:'TEXT'},{column:'status',dtype:'TEXT'}]));
+	brite.registerDao("todo",new brite.dao.SQLiteDao("Todos","id",[{column:'taskName',dtype:'TEXT'},{column:'startDate',dtype:'TEXT'},{column:'endDate',dtype:'TEXT'},{column:'status',dtype:'TEXT'},{column:'imageId',dtype:'INTEGER'}]));
+	brite.registerDao("image",new brite.dao.SQLiteDao("Image","id",[{column:'type',dtype:'TEXT'},{column:'url',dtype:'TEXT'},{column:'ext',dtype:'TEXT'}]));
 	
 })(jQuery);
 
